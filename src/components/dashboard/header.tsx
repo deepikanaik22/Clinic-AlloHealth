1dce801cc264b2d3b3426ea85a8f3f46c7bf3c77
Deepika K Naik <nagshree127@gmail.com>
2025-08-10 16:56:43 +0000

    make a toggle button to switch from light mode to dark mode

--- src/components/dashboard/header.tsx
+++ src/components/dashboard/header.tsx
@@ -3,6 +3,7 @@ import { SidebarTrigger } from "@/components/ui/sidebar";
 import { Button } from "@/components/ui/button";
 import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 import { Bell, Menu } from "lucide-react";
+import { ThemeToggle } from "../theme-toggle";
 
 type DashboardHeaderProps = {
   title: string;
@@ -10,7 +11,7 @@ type DashboardHeaderProps = {
 
 export function DashboardHeader({ title }: DashboardHeaderProps) {
   return (
-    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+    <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background/50 backdrop-blur-md px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
        <SidebarTrigger className="md:hidden" />
       <h1 className="text-lg font-semibold md:text-2xl">{title}</h1>
       <div className="ml-auto flex items-center gap-2">
@@ -18,6 +19,7 @@ export function DashboardHeader({ title }: DashboardHeaderProps) {
             <Bell className="h-4 w-4" />
             <span className="sr-only">Toggle notifications</span>
         </Button>
+        <ThemeToggle />
       </div>
     </header>
   );
